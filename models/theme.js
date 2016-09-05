3eaf7e803e605954dd032ef60f2896dd62e1df67
Elliot Brock <ebrock@Jeffreys-MacBook-Pro.local>
2016-09-04 20:29:34 -0400

    add suite and theme models for mongoose

---
+++ models/theme.js
@@ -0,0 +1,32 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var connection = mongoose.createConnection("mongodb://localhost/niji");
+
+var themeSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  target: {
+    // urxvt, xterm, termite, etc.
+    type: String,
+    required: false
+  },
+  tags: {
+    type: String[],
+    required: false
+  }
+  // author: {
+  //   type: mongoose.Schema.Types.ObjectId,
+  //   ref: 'User',
+  //   required: true
+  // }
+  // This is commented out until we have a User schema
+});
+
+module.exports = connection.model('Theme', themeSchema);
