6d0966ff8a54cbb4a059e17fb757cf487c96a9aa
Elliot Brock <nuclearcoconut86@gmail.com>
2016-09-05 13:40:37 -0400

    add suite posting route

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,16 +2,16 @@ var express = require('express');
 var multer = require('multer');
 
 var router = express.Router();
-var storage =   multer.diskStorage({
+var screenshotStorage =   multer.diskStorage({
   destination: function (req, file, callback) {
-    callback(null, './uploads');
+    callback(null, './uploads/screenshots');
   },
   filename: function (req, file, callback) {
     callback(null, req.body.name);
   }
 });
-var upload = multer({
-  storage: storage,
+var screenshotUpload = multer({
+  storage: screenshotStorage,
   fileFilter: function(req, file, callback){
     if(file.mimetype.match(/image\/.+/)){
       callback(null, true);
@@ -21,6 +21,26 @@ var upload = multer({
     }
   }
 }).single('screenshot');
+
+var wallpaperStorage =   multer.diskStorage({
+  destination: function (req, file, callback) {
+    callback(null, './uploads/wallpapers');
+  },
+  filename: function (req, file, callback) {
+    callback(null, req.body.name);
+  }
+});
+var wallpaperUpload = multer({
+  storage: wallpaperStorage,
+  fileFilter: function(req, file, callback){
+    if(file.mimetype.match(/image\/.+/)){
+      callback(null, true);
+    }
+    else{
+      callback(null, false);
+    }
+  }
+}).single('wallpaper');
 
 var authRoutes = require('./auth');
 
@@ -66,6 +86,61 @@ router.post('/theme', function(req, res, next){
     }
     res.status(201).json({
       message: 'Saved theme',
+      obj: result
+    });
+  });
+});
+
+router.post('/suite', function(req, res, next){
+  Suite.find({name: req.body.name}, function(err, doc){
+    if(err){
+      return res.status(404).json({
+        title: 'An error occurred',
+        error: err
+      });
+    }
+    if(doc){
+      return res.status(409).json({
+        message: 'A suite with that name already exists'
+      });
+    }
+  });
+  if(req.body.screenshot){
+    upload(req, res, function(err){
+      if(err){
+        res.status(404).json({
+          title: 'An error occurred',
+          error: err
+        });
+      }
+    });
+  }
+  if(req.body.wallpaper){
+    upload(req, res, function(err){
+      if(err){
+        res.status(404).json({
+          title: 'An error occurred',
+          error: err
+        });
+      }
+    });
+  }
+  var suite = new Suite({
+    name: req.body.name,
+    description: req.body.description,
+    wallpaper: req.body.name,
+    tags: req.body.tags,
+    yaml: req.body.yaml
+  });
+  suite.save(function(err, result){
+    if(err){
+      return res.status(404).json({
+        title: 'An error occurred',
+        error: err
+      });
+    }
+    res.status(201).json({
+      message: 'Saved suite',
       obj: result
     });
   });
