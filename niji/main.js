8ba742ba93c4989e690ecfe0215209394506490a
nuclearcoconut42 <nuclearcoconut42@gmail.com>
2016-09-06 07:55:58 -0400

    clean out unnecessary deps
    do things with deps

--- niji/main.js
+++ niji/main.js
@@ -1,27 +1,17 @@
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
-var logger = require('morgan');
 var bodyParser = require('body-parser');
-var cookieParser = require('cookie-parser');
 
 // var appRoutes = require('./routes/app');
 var apiRoutes = require('./routes/api');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'app/prod/hbs'));
-app.set('view engine', 'hbs');
-
-// app.use(favicon(path.join(__dirname, 'app/prod/img', 'favicon.ico')));
-app.use(logger('dev'));
+app.use(favicon(path.join(__dirname, 'app/prod/img', 'favicon.ico')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'app/prod/')));
-app.use(passport.initialize());
-app.use(passport.session());
 
 app.use(function(req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', '*');
